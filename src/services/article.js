bf1d880134f7b9a548f9c17bbe9f59ee21f1d9e9
pranjal <pranjalrana474@gmail.com>
2023-09-18 12:44:39 +0530

    ai_summarizer

--- src/services/article.js
+++ src/services/article.js
@@ -1,9 +1,22 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
+const options = {
+    method: 'GET',
+    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    params: {
+      url: 'https://time.com/6266679/musk-ai-open-letter/',
+      length: '3'
+    },
+    headers: {
+      'X-RapidAPI-Key': '922670b93cmshb6249229005b67ap1393cejsn978a77a2ddad',
+      'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+  };
+  
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl\: ''
+        baseUrl: ''
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
